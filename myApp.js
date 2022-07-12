39b1f6091ea853cad7922e7e886bf273f9e8fd74
Shing Fung Lam <shing2888@gmail.com>
2022-07-11 21:48:56 -0500

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,24 +2,16 @@ let express = require('express');
 let app = express();
 const dotenv = require('dotenv').config();
 const helloJson = "Hello json";
-let message;
 const filePath = __dirname + '/views/index.html'; 
-
-const MESSAGE_STYLE = process.env.MESSAGE_STYLE;
-
-console.log(MESSAGE_STYLE);
+let message;
 
 app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/json', (req, res) => {
   
-  if (process.env.MESSAGE_STYLE === 'uppercase')
-    message = helloJson.toUpperCase();
-  else
-    message = helloJson;
+  message = process.env.MESSAGE_STYLE === 'uppercase' ? helloJson.toUpperCase() : helloJson;
   
-    const data = { "message": message };
-  res.json(data);
+  res.json({ "message": message });
 })
 
 app.get('/', (req, res) => res.sendFile(filePath));
