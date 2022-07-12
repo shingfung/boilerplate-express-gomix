70c5422c9df5f31c0da55f1c48d696415dbe130f
Shing Fung Lam <shing2888@gmail.com>
2022-07-11 22:13:29 -0500

    Update

--- myApp.js
+++ myApp.js
@@ -5,6 +5,11 @@ const helloJson = "Hello json";
 const filePath = __dirname + '/views/index.html'; 
 let message;
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/json', (req, res) => {
