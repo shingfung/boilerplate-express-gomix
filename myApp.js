39fe4cf2c9283fbdc230d645a104232371e84ae6
Shing Fung Lam <shing2888@gmail.com>
2022-07-11 23:09:58 -0500

    Update

--- myApp.js
+++ myApp.js
@@ -17,6 +17,8 @@ app.get('/now', (req, res, next) => {
 
 app.get('/:word/echo', (req, res) => res.json({echo: req.params.word}));
 
+app.route('/name').get((req, res) => res.json({name: `${req.query.first} ${req.query.last}`}));
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/json', (req, res) => {
