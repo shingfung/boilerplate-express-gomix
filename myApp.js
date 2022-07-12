0f90f4a9a3cd8f9f34c8a0dad477cd47e0a80bc7
shingfung <shing2888@gmail.com>
2022-07-12 13:17:41 -0500

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -20,7 +20,12 @@ app.get('/now', (req, res, next) => {
 
 app.get('/:word/echo', (req, res) => res.json({echo: req.params.word}));
 
-app.route('/name').get((req, res) => res.json({name: `${req.query.first} ${req.query.last}`}));
+app.route('/name')
+.get((req, res) => res.json({name: `${req.query.first} ${req.query.last}`}))
+.post((req, res) => res.json({name: `${req.body.first} ${req.body.last}`}))
+;
+
+
 
 app.use('/public', express.static(__dirname + '/public'));
 
